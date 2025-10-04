cb6f108afc26b14230b2dab80d5a2d419a2d5210
Nitheesh D R <nitheeraj1@gmail.com>
2025-10-04 14:39:49 +0530

    new

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,16 +3,21 @@ import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Welcome to Resumind!" },
   ];
 }
 
 export default function Home() {
   return <main>
-    <section>
-      <div>
-        <p className="primary-gradient">Welcome to the Home Page!</p>
+    <nav>
+
+    </nav>
+    <section className="main-section" >
+      <div className="page-heading">
+        <h1>Track Your Apllication & Resume Rating</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+
       </div>
     </section>
   </main>
