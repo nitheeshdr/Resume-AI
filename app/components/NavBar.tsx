0ad32d04d1a304e3df454153458550f5367830af
Nitheesh D R <nitheeraj1@gmail.com>
2025-10-04 15:00:42 +0530

    new

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
         <Link to="/">
         <p className='text-2xl font-bold text-gradient' >RESUMIND</p>
         </Link>
-        <Link to="/upload"className='primary-button w-fit' >
+        <Link to="/upload"className='primaryButton w-fix' >
         Upload Resume
         </Link>
     </nav>
